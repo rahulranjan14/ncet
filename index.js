f775fae84925ded9fc72d8ff572d739efcc19a3d
rahulranjan14 <rahulranjan1680@gmail.com>
2022-06-03 20:38:21 +0530

    changes

--- index.js
+++ index.js
@@ -63,15 +63,14 @@ app.listen(port, () => {
 
 //production
 
-//  if(process.env.NODE_ENV === "production"){
-//    app.use(express.static('client/build'));
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
 
-//    app.get('*', (req, res) => {
-//      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//  })
-//  }
-
-// Making Build Folder as Public 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+}
+// // Making Build Folder as Public 
 // app.use(express.static(path.join(__dirname, 'build')));
 
 // app.get('/', function(req, res) {
